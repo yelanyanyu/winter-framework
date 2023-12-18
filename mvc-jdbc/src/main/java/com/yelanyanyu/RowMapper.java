6572801ca7945833daf93c666b2a69e60df83486
yelanyanyu <yelanyanyu@outlook.com>
2023-12-18 16:39:17 +0800

    feat: JdbcTemplate.queryForObject
    date: 2023/12/11

--- mvc-jdbc/src/main/java/com/yelanyanyu/RowMapper.java
+++ mvc-jdbc/src/main/java/com/yelanyanyu/RowMapper.java
@@ -6,7 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Callback interface for JDBC code. To be used with JdbcTemplate's execution methods, often as anonymous classes
+ * Callback interface for JDBC code. To be used with JdbcTemplate's execution methods, often as anonymous classes.
+ * When database's work done, you can use this interface to convert the result set to any type in custom way. There are three default implementations in JdbcTemplate, such as BeanRowMapper, NumberRowMapper and StringRowMapper, you can use them directly.
  *
  * @author yelanyanyu@zjxu.edu.cn
  * @version 1.0
